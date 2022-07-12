d46ae855e42b61348023154e20bdac5d4f4d0fc2
MM-WebDeveloper <milan.varnamo@gmail.com>
2022-07-12 22:05:14 +0200

    added sass

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import './categories.styles.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	const categories = [
+		{
+			id: 1,
+			title: 'Guitars',
+		},
+		{
+			id: 2,
+			title: 'Basses',
+		},
+	];
+	return (
+		<div className='categories-container'>
+			{categories.map((category) => (
+				<div className='category-container' key={category.id}>
+					<div className='background-image' />
+					<div className='category-body-container'>
+						<h2>{category.title}</h2>
+						<p>Shop Now</p>
+					</div>
+				</div>
+			))}
+		</div>
+	);
+};
 
 export default App;
